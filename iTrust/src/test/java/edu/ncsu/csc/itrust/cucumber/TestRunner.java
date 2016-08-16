d10569a01a746517fce1eecef9e76d3d0cb9e567
seelder <seelder@ncsu.edu>
2016-08-16 13:57:00 -0400

    Updated testRunner so that random cucumber file isn't created.
    Tests run clean. Server starts cleanly. Some CSS warnings when tests are
    run (existing)

--- iTrust/src/test/java/edu/ncsu/csc/itrust/cucumber/TestRunner.java
+++ iTrust/src/test/java/edu/ncsu/csc/itrust/cucumber/TestRunner.java
@@ -6,7 +6,7 @@ import cucumber.api.CucumberOptions;
 import cucumber.api.junit.Cucumber;
 
 @RunWith(Cucumber.class)
-@CucumberOptions(features="src/test/resources/edu/ncsu/csc/itrust/cucumber",plugin = {"pretty", "junit:\\iTrust\\cucumberTest.xml"})
+@CucumberOptions(features="src/test/resources/edu/ncsu/csc/itrust/cucumber")
 public class TestRunner {
 	
 }
