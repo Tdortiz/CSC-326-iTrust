6ae52db3ace571027efc0b3083068c919454febd
Xiaohang Miao <xmiao2@ncsu.edu>
2016-09-21 11:51:51 -0400

    Added skeleton for BasicHealthInfo step definitions

--- iTrust/src/test/java/edu/ncsu/csc/itrust/cucumber/BasicHealthInfoStepDefs.java
+++ iTrust/src/test/java/edu/ncsu/csc/itrust/cucumber/BasicHealthInfoStepDefs.java
@@ -1,7 +1,312 @@
 package edu.ncsu.csc.itrust.cucumber;
 
+import cucumber.api.PendingException;
+import cucumber.api.java.en.Given;
+import cucumber.api.java.en.Then;
+import cucumber.api.java.en.When;
+
 public class BasicHealthInfoStepDefs {
 	public BasicHealthInfoStepDefs() {
 		
+	}
+	
+	@Given("^Shelly Vang is an HCP with MID: (\\d+)$")
+	public void shelly_Vang_is_an_HCP_with_MID(int arg1) throws Throwable {
+	    // Write code here that turns the phrase above into concrete actions
+	    throw new PendingException();
+	}
+
+	@Given("^Brynn McClain is a patient with MID (\\d+) who is (\\d+) months old$")
+	public void brynn_McClain_is_a_patient_with_MID_who_is_months_old(int arg1, int arg2) throws Throwable {
+	    // Write code here that turns the phrase above into concrete actions
+	    throw new PendingException();
+	}
+
+	@When("^Shelly Vang logs in$")
+	public void shelly_Vang_logs_in() throws Throwable {
+	    // Write code here that turns the phrase above into concrete actions
+	    throw new PendingException();
+	}
+
+	@When("^Chooses to document an office visit$")
+	public void chooses_to_document_an_office_visit() throws Throwable {
+	    // Write code here that turns the phrase above into concrete actions
+	    throw new PendingException();
+	}
+
+	@When("^selects patient Brynn McClain with MID (\\d+)$")
+	public void selects_patient_Brynn_McClain_with_MID(int arg1) throws Throwable {
+	    // Write code here that turns the phrase above into concrete actions
+	    throw new PendingException();
+	}
+
+	@When("^Choose appointment type General checkup$")
+	public void choose_appointment_type_General_checkup() throws Throwable {
+	    // Write code here that turns the phrase above into concrete actions
+	    throw new PendingException();
+	}
+
+	@When("^Chooses the date to be (\\d+)/(\\d+)/(\\d+)$")
+	public void chooses_the_date_to_be(int arg1, int arg2, int arg3) throws Throwable {
+	    // Write code here that turns the phrase above into concrete actions
+	    throw new PendingException();
+	}
+
+	@When("^Select Central Hospital for location$")
+	public void select_Central_Hospital_for_location() throws Throwable {
+	    // Write code here that turns the phrase above into concrete actions
+	    throw new PendingException();
+	}
+
+	@When("^enters Notes “Brynn can start eating rice cereal mixed with breast milk or formula once a day”$")
+	public void enters_Notes_Brynn_can_start_eating_rice_cereal_mixed_with_breast_milk_or_formula_once_a_day() throws Throwable {
+	    // Write code here that turns the phrase above into concrete actions
+	    throw new PendingException();
+	}
+
+	@When("^Creates a Basic Health History$")
+	public void creates_a_Basic_Health_History() throws Throwable {
+	    // Write code here that turns the phrase above into concrete actions
+	    throw new PendingException();
+	}
+
+	@When("^Enters (\\d+)\\.(\\d+) for weight$")
+	public void enters_for_weight(int arg1, int arg2) throws Throwable {
+	    // Write code here that turns the phrase above into concrete actions
+	    throw new PendingException();
+	}
+
+	@When("^Enters (\\d+)\\.(\\d+) for length$")
+	public void enters_for_length(int arg1, int arg2) throws Throwable {
+	    // Write code here that turns the phrase above into concrete actions
+	    throw new PendingException();
+	}
+
+	@When("^Enters (\\d+)\\.(\\d+) for Head Circumference$")
+	public void enters_for_Head_Circumference(int arg1, int arg2) throws Throwable {
+	    // Write code here that turns the phrase above into concrete actions
+	    throw new PendingException();
+	}
+
+	@When("^selects (\\d+) for non-smoking for household smoking status$")
+	public void selects_for_non_smoking_for_household_smoking_status(int arg1) throws Throwable {
+	    // Write code here that turns the phrase above into concrete actions
+	    throw new PendingException();
+	}
+
+	@When("^submits record$")
+	public void submits_record() throws Throwable {
+	    // Write code here that turns the phrase above into concrete actions
+	    throw new PendingException();
+	}
+
+	@Then("^a success message is displayed$")
+	public void a_success_message_is_displayed() throws Throwable {
+	    // Write code here that turns the phrase above into concrete actions
+	    throw new PendingException();
+	}
+
+	@Given("^Caldwell Hudson is a patient with MID (\\d+) who is two years old$")
+	public void caldwell_Hudson_is_a_patient_with_MID_who_is_two_years_old(int arg1) throws Throwable {
+	    // Write code here that turns the phrase above into concrete actions
+	    throw new PendingException();
+	}
+
+	@When("^selects patient Caldwell Hudson MID (\\d+)$")
+	public void selects_patient_Caldwell_Hudson_MID(int arg1) throws Throwable {
+	    // Write code here that turns the phrase above into concrete actions
+	    throw new PendingException();
+	}
+
+	@When("^enters Note “Diagnosed with strep throat\\.  Avoid contact with others for the first (\\d+) hours of antibiotics”$")
+	public void enters_Note_Diagnosed_with_strep_throat_Avoid_contact_with_others_for_the_first_hours_of_antibiotics(int arg1) throws Throwable {
+	    // Write code here that turns the phrase above into concrete actions
+	    throw new PendingException();
+	}
+
+	@When("^selects (\\d+) for indoors smokers for household smoking status$")
+	public void selects_for_indoors_smokers_for_household_smoking_status(int arg1) throws Throwable {
+	    // Write code here that turns the phrase above into concrete actions
+	    throw new PendingException();
+	}
+
+	@Given("^Fulton Gray is a patient with MID (\\d+) who is five years old$")
+	public void fulton_Gray_is_a_patient_with_MID_who_is_five_years_old(int arg1) throws Throwable {
+	    // Write code here that turns the phrase above into concrete actions
+	    throw new PendingException();
+	}
+
+	@When("^selects patient Fulton Gray MID (\\d+)$")
+	public void selects_patient_Fulton_Gray_MID(int arg1) throws Throwable {
+	    // Write code here that turns the phrase above into concrete actions
+	    throw new PendingException();
+	}
+
+	@When("^enters Note “Fulton has all required immunizations to start kindergarten next year\\.”$")
+	public void enters_Note_Fulton_has_all_required_immunizations_to_start_kindergarten_next_year() throws Throwable {
+	    // Write code here that turns the phrase above into concrete actions
+	    throw new PendingException();
+	}
+
+	@When("^Enters (\\d+)\\.(\\d+) for height$")
+	public void enters_for_height(int arg1, int arg2) throws Throwable {
+	    // Write code here that turns the phrase above into concrete actions
+	    throw new PendingException();
+	}
+
+	@When("^Enters (\\d+)/(\\d+) for blood pressure$")
+	public void enters_for_blood_pressure(int arg1, int arg2) throws Throwable {
+	    // Write code here that turns the phrase above into concrete actions
+	    throw new PendingException();
+	}
+
+	@When("^selects (\\d+) for outdoor smokers for household smoking status$")
+	public void selects_for_outdoor_smokers_for_household_smoking_status(int arg1) throws Throwable {
+	    // Write code here that turns the phrase above into concrete actions
+	    throw new PendingException();
+	}
+
+	@Given("^Daria Griffin is a patient with MID (\\d+) who is (\\d+) years old$")
+	public void daria_Griffin_is_a_patient_with_MID_who_is_years_old(int arg1, int arg2) throws Throwable {
+	    // Write code here that turns the phrase above into concrete actions
+	    throw new PendingException();
+	}
+
+	@When("^enters Note “Patient is healthy\\.”$")
+	public void enters_Note_Patient_is_healthy() throws Throwable {
+	    // Write code here that turns the phrase above into concrete actions
+	    throw new PendingException();
+	}
+
+	@When("^selects (\\d+) for former smoker for patient smoking status$")
+	public void selects_for_former_smoker_for_patient_smoking_status(int arg1) throws Throwable {
+	    // Write code here that turns the phrase above into concrete actions
+	    throw new PendingException();
+	}
+
+	@When("^enters (\\d+) for HDL$")
+	public void enters_for_HDL(int arg1) throws Throwable {
+	    // Write code here that turns the phrase above into concrete actions
+	    throw new PendingException();
+	}
+
+	@When("^enters (\\d+) for LDL$")
+	public void enters_for_LDL(int arg1) throws Throwable {
+	    // Write code here that turns the phrase above into concrete actions
+	    throw new PendingException();
+	}
+
+	@When("^enters (\\d+) for Triglycerides$")
+	public void enters_for_Triglycerides(int arg1) throws Throwable {
+	    // Write code here that turns the phrase above into concrete actions
+	    throw new PendingException();
+	}
+
+	@Given("^Thane Ross is a patient with MID (\\d+) who is (\\d+) years old$")
+	public void thane_Ross_is_a_patient_with_MID_who_is_years_old(int arg1, int arg2) throws Throwable {
+	    // Write code here that turns the phrase above into concrete actions
+	    throw new PendingException();
+	}
+
+	@When("^selects patient Thane Ross MID (\\d+)$")
+	public void selects_patient_Thane_Ross_MID(int arg1) throws Throwable {
+	    // Write code here that turns the phrase above into concrete actions
+	    throw new PendingException();
+	}
+
+	@When("^enters Note “Thane should consider modifying diet and exercise to avoid future heart disease”$")
+	public void enters_Note_Thane_should_consider_modifying_diet_and_exercise_to_avoid_future_heart_disease() throws Throwable {
+	    // Write code here that turns the phrase above into concrete actions
+	    throw new PendingException();
+	}
+
+	@When("^selects (\\d+) for never smoked for patient smoking status$")
+	public void selects_for_never_smoked_for_patient_smoking_status(int arg1) throws Throwable {
+	    // Write code here that turns the phrase above into concrete actions
+	    throw new PendingException();
+	}
+
+	@Given("^Kelly Doctor is an HCP with MID: (\\d+)$")
+	public void kelly_Doctor_is_an_HCP_with_MID(int arg1) throws Throwable {
+	    // Write code here that turns the phrase above into concrete actions
+	    throw new PendingException();
+	}
+
+	@Given("^Random Person is a patient with MID (\\d+)$")
+	public void random_Person_is_a_patient_with_MID(int arg1) throws Throwable {
+	    // Write code here that turns the phrase above into concrete actions
+	    throw new PendingException();
+	}
+
+	@When("^selects patient Random Person MID (\\d+)$")
+	public void selects_patient_Random_Person_MID(int arg1) throws Throwable {
+	    // Write code here that turns the phrase above into concrete actions
+	    throw new PendingException();
+	}
+
+	@When("^enters Note “Came in complaining of splinters\\.”$")
+	public void enters_Note_Came_in_complaining_of_splinters() throws Throwable {
+	    // Write code here that turns the phrase above into concrete actions
+	    throw new PendingException();
+	}
+
+	@When("^Enters (\\d+) for weight$")
+	public void enters_for_weight(int arg1) throws Throwable {
+	    // Write code here that turns the phrase above into concrete actions
+	    throw new PendingException();
+	}
+
+	@Then("^a error message is displayed$")
+	public void a_error_message_is_displayed() throws Throwable {
+	    // Write code here that turns the phrase above into concrete actions
+	    throw new PendingException();
+	}
+
+	@When("^enters Note “It doesn't matter”$")
+	public void enters_Note_It_doesn_t_matter() throws Throwable {
+	    // Write code here that turns the phrase above into concrete actions
+	    throw new PendingException();
+	}
+
+	@When("^Enters /(\\d+) for blood pressure   \\[high bp is empty\\]$")
+	public void enters_for_blood_pressure_high_bp_is_empty(int arg1) throws Throwable {
+	    // Write code here that turns the phrase above into concrete actions
+	    throw new PendingException();
+	}
+
+	@When("^selects (\\d+) for new smoked for patient smoking status$")
+	public void selects_for_new_smoked_for_patient_smoking_status(int arg1) throws Throwable {
+	    // Write code here that turns the phrase above into concrete actions
+	    throw new PendingException();
+	}
+
+	@Then("^an error message is displayed$")
+	public void an_error_message_is_displayed() throws Throwable {
+	    // Write code here that turns the phrase above into concrete actions
+	    throw new PendingException();
+	}
+
+	@When("^enters Note “Testing invalid input”$")
+	public void enters_Note_Testing_invalid_input() throws Throwable {
+	    // Write code here that turns the phrase above into concrete actions
+	    throw new PendingException();
+	}
+
+	@When("^enters AA for HDL$")
+	public void enters_AA_for_HDL() throws Throwable {
+	    // Write code here that turns the phrase above into concrete actions
+	    throw new PendingException();
+	}
+
+	@When("^enters Note “Brynn is a very healthy baby”$")
+	public void enters_Note_Brynn_is_a_very_healthy_baby() throws Throwable {
+	    // Write code here that turns the phrase above into concrete actions
+	    throw new PendingException();
+	}
+
+	@When("^Enters (\\d+) for Head Circumference$")
+	public void enters_for_Head_Circumference(int arg1) throws Throwable {
+	    // Write code here that turns the phrase above into concrete actions
+	    throw new PendingException();
 	}
 }
