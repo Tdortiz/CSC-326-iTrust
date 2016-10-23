35785b98dddce5e14a882ef005db5fd9c214df0c
Nathan Miles <nmiles@ncsu.edu>
2016-10-23 16:47:32 -0400

    Added EmergencyRecordTest with 100% coverage
    Allergy, diagnosis, prescription, and immunization functionality tests
    won't be useful until that functionality is added.

---
+++ iTrust/src/test/java/edu/ncsu/csc/itrust/unit/model/emergencyRecord/EmergencyRecordTest.java
@@ -0,0 +1,83 @@
+package edu.ncsu.csc.itrust.unit.model.emergencyRecord;
+
+import org.junit.Assert;
+import org.junit.Test;
+
+import edu.ncsu.csc.itrust.model.emergencyRecord.EmergencyRecord;
+import edu.ncsu.csc.itrust.model.old.enums.BloodType;
+import edu.ncsu.csc.itrust.model.old.enums.Gender;
+import junit.framework.TestCase;
+
+public class EmergencyRecordTest extends TestCase {
+    
+    EmergencyRecord r;
+    
+    @Override
+    public void setUp(){
+        r = new EmergencyRecord();
+    }
+    
+    @Test
+    public void testName(){
+        r.setName("testName");
+        Assert.assertEquals("testName", r.getName());
+    }
+    
+    @Test
+    public void testAge(){
+        r.setAge(50);
+        Assert.assertEquals(50,  r.getAge());
+    }
+    
+    @Test
+    public void testGender(){
+        r.setGender(Gender.Female);
+        Assert.assertEquals(Gender.Female, r.getGender());
+    }
+    
+    @Test
+    public void testContactName(){
+        r.setContactName("cName");
+        Assert.assertEquals("cName", r.getContactName());
+    }
+    
+    @Test
+    public void testContactPhone(){
+        r.setContactPhone("999-555-1111");
+        Assert.assertEquals("999-555-1111", r.getContactPhone());
+    }
+    
+    //TODO: fix this when allergy functionality is added
+    @Test
+    public void testAllergies(){
+        r.setAllergies(null);
+        Assert.assertNull(r.getAllergies());
+    }
+    
+    @Test
+    public void testBloodType(){
+        r.setBloodType(BloodType.ANeg);
+        Assert.assertEquals(BloodType.ANeg, r.getBloodType());
+    }
+    
+    //TODO: fix this when diagnosis functionality is added
+    @Test
+    public void testDiagnoses(){
+        r.setDiagnoses(null);
+        Assert.assertNull(r.getDiagnoses());
+    }
+    
+    //TODO: fix this when prescription functionality is added
+    @Test
+    public void testPrescriptions(){
+        r.setPrescriptions(null);
+        Assert.assertNull(r.getPrescriptions());
+    }
+    
+    //TODO: fix this when immunization functionality is added
+    @Test
+    public void testImmunizations(){
+        r.setImmunizations(null);
+        Assert.assertNull(r.getImmunizations());
+    }
+}
