4b05e2c2e3e2e92228b35bb223f0aa0f244c04c7
Nathan Miles <nmiles@ncsu.edu>
2016-10-23 15:07:18 -0400

    Added all fields to EmergencyRecord
    Allergies, diagnoses, prescriptions, and immunizations are currently empty
    lists, as we don't have those functions implemented yet. These fields are
    stubs for now.

--- iTrust/src/main/edu/ncsu/csc/itrust/model/emergencyRecord/EmergencyRecord.java
+++ iTrust/src/main/edu/ncsu/csc/itrust/model/emergencyRecord/EmergencyRecord.java
@@ -1,5 +1,24 @@
 package edu.ncsu.csc.itrust.model.emergencyRecord;
 
-public class EmergencyRecord {
+import java.util.List;
 
+import edu.ncsu.csc.itrust.model.old.enums.BloodType;
+import edu.ncsu.csc.itrust.model.old.enums.Gender;
+
+/**
+ * A class for storing emergency health record data for UC21
+ * @author nmiles
+ *
+ */
+public class EmergencyRecord {
+    private String name;
+    private int age;
+    private Gender gender;
+    private String contactName;
+    private String contactPhone;
+    private List<Object> allergies;
+    private BloodType bloodType;
+    private List<Object> diagnoses;
+    private List<Object> prescriptions;
+    private List<Object> immunizations;
 }
