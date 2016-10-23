dd8e2f5205b62118216eff116931c366536a0d9a
Nathan Miles <nmiles@ncsu.edu>
2016-10-23 16:22:07 -0400

    Added skeleton of EmergencyRecordController
    Stub for loadRecord()

--- iTrust/src/main/edu/ncsu/csc/itrust/controller/emergencyRecord/EmergencyRecordController.java
+++ iTrust/src/main/edu/ncsu/csc/itrust/controller/emergencyRecord/EmergencyRecordController.java
@@ -1,5 +1,45 @@
 package edu.ncsu.csc.itrust.controller.emergencyRecord;
 
-public class EmergencyRecordController {
+import javax.faces.bean.ManagedBean;
+import javax.faces.bean.SessionScoped;
 
+import edu.ncsu.csc.itrust.model.emergencyRecord.EmergencyRecord;
+import edu.ncsu.csc.itrust.model.old.dao.DAOFactory;
+import edu.ncsu.csc.itrust.model.old.dao.mysql.PatientDAO;
+
+/**
+ * A controller class for EmergencyRecord
+ * @author nmiles
+ *
+ */
+@ManagedBean(name="emergency_record_controller")
+@SessionScoped
+public class EmergencyRecordController {
+    private PatientDAO patientDAO;
+    private EmergencyRecord record;
+    
+    /**
+     * Constructs a new EmergencyRecordController
+     */
+    public EmergencyRecordController(){
+        patientDAO = new PatientDAO(DAOFactory.getProductionInstance());
+        record = new EmergencyRecord();
+    }
+    
+    /**
+     * Gets the Emergency Record
+     * @return
+     */
+    public EmergencyRecord getRecord(){
+        return record;
+    }
+    
+    /**
+     * Stub
+     * @param mid
+     * @return
+     */
+    public boolean loadRecord(long mid){
+        return false;
+    }
 }
