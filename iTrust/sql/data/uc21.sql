cb898a70b49e1bd9fb25c2302fde75de14c22d04
Nathan Miles <nmiles@ncsu.edu>
2016-10-25 15:52:35 -0400

    Added officeVisit and prescription codes for testing

--- iTrust/sql/data/uc21.sql
+++ iTrust/sql/data/uc21.sql
@@ -66,3 +66,34 @@ INSERT INTO users(MID, password, role, sQuestion, sAnswer)
 			VALUES (201, '30c952fab122c3f9759f02a6d95c3758b246b4fee239957b2d4fee46e26170c4', 'patient', '2+2?', '4')
  ON DUPLICATE KEY UPDATE MID = MID;
  /*password: pw*/
+ 
+ /* Office visit for Sandy Sky */
+ INSERT INTO officevisit (
+ 	visitID,
+	patientMID, 
+	visitDate, 
+	locationID, 
+	apptTypeID, 
+	weight, 
+	height,
+	blood_pressure,
+	household_smoking_status,
+	patient_smoking_status,
+	hdl,
+	ldl,
+	triglyceride) 
+VALUES (2100, 201, DATE(NOW()-INTERVAL 1 YEAR), 1, 1, 180, 70, '100/70', 1, 4, 40, 81, 105);
+
+/* Prescription codes for SandySky */
+INSERT INTO ndcodes (
+	Code,
+	Description)
+VALUES (05730150, "Advil");
+INSERT INTO ndcodes (
+	Code,
+	Description)
+VALUES (483013420, "Midichlomaxene");
+INSERT INTO ndcodes (
+	Code,
+	Description)
+VALUES (63739291, "Oyster Shell Calcium with Vitamin D");
