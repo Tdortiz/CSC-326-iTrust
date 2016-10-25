be8875b72e3a119758ef8f53878241b8af6d61b6
mwreesjo <mwreesjo@ncsu.edu>
2016-10-24 23:06:56 -0400

    Add LabProcedureController unit tests

---
+++ iTrust/sql/data/labProcedure1.sql
@@ -0,0 +1,19 @@
+INSERT INTO labProcedure (
+	labTechnicianID,
+	officeVisitID,
+	priority,
+	isRestricted,
+	status,
+	commentary,
+	results,
+	updatedDate
+) VALUES (
+	5000000001,
+	4,
+	1,
+	true,
+	2,
+	"A hi-pri lab procedure",
+	"Results are important!",
+	"2016-10-31 09:30:00"
+);
---
+++ iTrust/sql/data/labProcedure5.sql
@@ -0,0 +1,19 @@
+INSERT INTO labProcedure (
+	labTechnicianID,
+	officeVisitID,
+	priority,
+	isRestricted,
+	status,
+	commentary,
+	results,
+	updatedDate
+) VALUES (
+	5000000002,
+	4,
+	3,
+	true,
+	1,
+	"This is for lab tech 5000000002",
+	"Foobar",
+	"2013-10-31 11:00:00"
+);
