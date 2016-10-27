0c92ad76d3e436575ec1e110a735fb21677c0c3d
mmackre <mmackre@ncsu.edu>
2016-10-26 23:37:58 -0400

    Final fix

--- iTrust/sql/data/uc21.sql
+++ iTrust/sql/data/uc21.sql
@@ -328,3 +328,7 @@ NULL,
 'This guy is a hippy.'
 )  ON DUPLICATE KEY UPDATE MID = MID;
 
+INSERT INTO users(MID, password, role, sQuestion, sAnswer) 
+			VALUES (105, '30c952fab122c3f9759f02a6d95c3758b246b4fee239957b2d4fee46e26170c4', 'patient', '2+2?', '4')
+ ON DUPLICATE KEY UPDATE MID = MID;
+
