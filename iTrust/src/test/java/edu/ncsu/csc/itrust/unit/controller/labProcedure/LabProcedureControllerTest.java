b2254676dd7add6e38d5144e28b35556d5776e0b
xmiao2 <xmiao2@ncsu.edu>
2016-11-01 17:14:59 -0400

    Fixed junit tests error caused by getLabProcedureId

--- iTrust/src/test/java/edu/ncsu/csc/itrust/unit/controller/labProcedure/LabProcedureControllerTest.java
+++ iTrust/src/test/java/edu/ncsu/csc/itrust/unit/controller/labProcedure/LabProcedureControllerTest.java
@@ -1,13 +1,13 @@
 package edu.ncsu.csc.itrust.unit.controller.labProcedure;
 
 import static org.junit.Assert.fail;
+import static org.mockito.Matchers.any;
+import static org.mockito.Matchers.eq;
 import static org.mockito.Mockito.mock;
 import static org.mockito.Mockito.spy;
 import static org.mockito.Mockito.times;
 import static org.mockito.Mockito.verify;
 import static org.mockito.Mockito.when;
-import static org.mockito.Mockito.any;
-import static org.mockito.Mockito.eq;
 
 import java.io.FileNotFoundException;
 import java.io.IOException;
@@ -16,7 +16,6 @@ import java.sql.Timestamp;
 import java.util.List;
 
 import javax.faces.application.FacesMessage;
-import javax.print.attribute.standard.Severity;
 import javax.sql.DataSource;
 
 import org.junit.After;
@@ -29,8 +28,8 @@ import edu.ncsu.csc.itrust.controller.labProcedure.LabProcedureController;
 import edu.ncsu.csc.itrust.exception.DBException;
 import edu.ncsu.csc.itrust.model.ConverterDAO;
 import edu.ncsu.csc.itrust.model.labProcedure.LabProcedure;
-import edu.ncsu.csc.itrust.model.labProcedure.LabProcedureData;
 import edu.ncsu.csc.itrust.model.labProcedure.LabProcedure.LabProcedureStatus;
+import edu.ncsu.csc.itrust.model.labProcedure.LabProcedureData;
 import edu.ncsu.csc.itrust.unit.datagenerators.TestDataGenerator;
 
 /**
@@ -455,12 +454,12 @@ public class LabProcedureControllerTest {
 		gen.labProcedure0();
 		controller = spy(controller);
 		Mockito.doNothing().when(controller).printFacesMessage(any(), any(), any(), any());
-		LabProcedure proc = controller.get("1");
+		LabProcedure proc = controller.getLabProcedureByID("1");
 		Assert.assertNotNull(proc);
 		Assert.assertEquals(5000000001L, proc.getLabTechnicianID().longValue());
-		proc = controller.get("-1");
+		proc = controller.getLabProcedureByID("-1");
 		Assert.assertNull(proc);
-		proc = controller.get("aa");
+		proc = controller.getLabProcedureByID("aa");
 		Assert.assertNull(proc);
 		verify(controller).printFacesMessage(eq(FacesMessage.SEVERITY_ERROR), any(), any(), any());
 	}
