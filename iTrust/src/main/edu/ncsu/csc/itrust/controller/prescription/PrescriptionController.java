6968dd3efbc6d835ce399f80ff7555c3dad84953
Thomas Ortiz <tdortiz@ncsu.edu>
2016-11-06 20:45:27 -0500

    UC19: Basic UI for hcp

---
+++ iTrust/src/main/edu/ncsu/csc/itrust/controller/prescription/PrescriptionController.java
@@ -0,0 +1,10 @@
+package edu.ncsu.csc.itrust.controller.prescription;
+
+import javax.faces.bean.ManagedBean;
+import javax.faces.bean.SessionScoped;
+
+@ManagedBean(name = "prescription_controller")
+@SessionScoped
+public class PrescriptionController {
+
+}
