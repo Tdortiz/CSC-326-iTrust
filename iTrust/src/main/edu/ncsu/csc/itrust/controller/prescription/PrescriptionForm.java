6df50304bb55cfd7ec2b5ee965d81e097768f6aa
xmiao2 <xmiao2@ncsu.edu>
2016-11-15 01:19:13 -0500

    Fixed bug that prevented prescriptions from being added under the correct ov, hcp and patient

--- iTrust/src/main/edu/ncsu/csc/itrust/controller/prescription/PrescriptionForm.java
+++ iTrust/src/main/edu/ncsu/csc/itrust/controller/prescription/PrescriptionForm.java
@@ -41,7 +41,7 @@ public class PrescriptionForm {
 				ndcData = (nData == null) ? new NDCCodeMySQL(ds) : nData;
 				controller = (pc == null) ? new PrescriptionController(ds) : pc;
 			}
-			prescription = new Prescription();
+			clearFields();
 			
 		} catch (Exception e) {
 			this.sessionUtils.printFacesMessage(FacesMessage.SEVERITY_ERROR, "Prescription Controller Error",
@@ -124,6 +124,8 @@ public class PrescriptionForm {
 	
 	public void clearFields(){
 		this.prescription = new Prescription();
+		prescription.setPatientMID(sessionUtils.getCurrentPatientMIDLong());
+		prescription.setOfficeVisitId(sessionUtils.getCurrentOfficeVisitId());
+		prescription.setHcpMID(sessionUtils.getSessionLoggedInMIDLong());
 	}
-
 }
