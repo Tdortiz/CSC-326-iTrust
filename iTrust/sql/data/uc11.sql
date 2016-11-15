608e8eb79c040a8f7515a5238f3217ca12ebcf48
mmackre <mmackre@ncsu.edu>
2016-11-14 22:06:20 -0500

    All uc11 black box and acc tests pass

--- iTrust/sql/data/uc11.sql
+++ iTrust/sql/data/uc11.sql
@@ -18,20 +18,20 @@ VALUES (26, "2015-11-13 00:00:00", 9191919191, 1, "Overall Good!", 163, 69, '102
 
 set @ov_id = LAST_INSERT_ID();
 
-INSERT INTO diagnosis (visitId, icdCode)
-VALUES (@ov_id, 'S60.7')
-ON DUPLICATE KEY UPDATE icdCode = icdCode;
+INSERT INTO icdCode (code, name, is_chronic)
+VALUES ('S607','Ouch on toe', 0)
+ON DUPLICATE KEY UPDATE code = code;
 
 INSERT INTO cptCode(code, name) 
 VALUES ('99214', ''), ('90717', 'Typhoid Vaccine')
 ON DUPLICATE KEY UPDATE code = code;
 
 INSERT INTO icdCode(code, name, is_chronic) VALUES
-('S10.8','Skin injury neck', 0)
+('S108','Skin injury neck', 0)
 ON DUPLICATE KEY UPDATE code = code;
 
 INSERT INTO icdCode(code, name, is_chronic) VALUES
-('S10.7','Skin injury hand', 0)
+('S107','Skin injury hand', 0)
 ON DUPLICATE KEY UPDATE code = code;
 
 INSERT INTO loincCode (code, component, kind_of_property, time_aspect, system, scale_type, method_type) VALUES
